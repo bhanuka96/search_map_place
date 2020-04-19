9210f5dd2b167cbbafe51c7a5958fecbf9148f74
Bhanuka Sampath <bhanukas@orelit.com>
2020-04-19 12:02:46 +0530

    getGeolocationByLatLng added

--- lib/src/geocoding.dart
+++ lib/src/geocoding.dart
@@ -21,4 +21,21 @@ class Geocoding {
       throw Exception(error);
     }
   }
+
+  Future<dynamic> getGeolocationByLatLng(LatLng latLng) async {
+    final url =
+        "https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLng.latitude},${latLng.longitude}&key=$apiKey&language=$language";
+    final response = await http.get(url);
+    final json = JSON.jsonDecode(response.body);
+
+    if (json["error_message"] == null) {
+      return Geolocation.fromJSON(json);
+    } else {
+      var error = json["error_message"];
+      if (error == "This API project is not authorized to use this API.")
+        error +=
+        " Make sure both the Geolocation and Geocoding APIs are activated on your Google Cloud Platform";
+      throw Exception(error);
+    }
+  }
 }
